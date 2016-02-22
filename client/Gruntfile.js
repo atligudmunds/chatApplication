fdc65b91a4dc3de5751677d2e0ce4e591234a4bc
Ragna Lára Ellertsdóttir <ragnalara@gmail.com>
2016-02-22 17:53:57 +0000

    watch virkar í task

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -2,12 +2,19 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	var taskConfig = {
 		jshint: {
-			//src: ['**/*.js'],
-			//gruntfile: ['Gruntfile.js'],
-			//options: {
-			//}
 			all: ['Gruntfile.js' , 'src/**/*.js']
-		}
+		},
+		watch: {
+        	files: ['Gruntfile.js', 'src/**/*.js'],
+        	tasks: ['jshint']
+    }
 	};
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	//grunt.registerTask('default', ['jshint']);
+	//grunt.registerTask('default', ['watch']);
+
+
 	grunt.initConfig ( taskConfig );
+
 };
